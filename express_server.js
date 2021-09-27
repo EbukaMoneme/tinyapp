ed6a161a38bbc97ee6ee1a5bea2ca7bdf324a7f1
EbukaMoneme <camoneme@gmail.com>
2021-09-27 20:28:17 +0000

    Showed the short and long form of urls using an ejs template

--- express_server.js
+++ express_server.js
@@ -16,9 +16,14 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
