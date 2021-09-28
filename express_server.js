b26de212e3b2801c59d728ac929cc8a7e3ca554b
EbukaMoneme <camoneme@gmail.com>
2021-09-28 00:19:31 +0000

    Fixed longURL display in urls_show

--- express_server.js
+++ express_server.js
@@ -4,6 +4,10 @@ const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+const newString = ()  => {
+  return Math.random().toString(36).substr(2, 6);
+}
+
 app.set("view engine", "ejs");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -28,13 +32,18 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase.shortURL };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls", (req, res) => {
+  let short = newString();
+  let long = req.body.longURL;
+  urlDatabase[short] = long;
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  console.log(urlDatabase);  // Log the POST request body to the console
+  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${short}`);
 });
 
 // app.get("/hello", (req, res) => {
@@ -45,6 +54,3 @@ app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
 
-function generateRandomString() {
-  return Math.random().toString(36).substr(2, 6);
-}
